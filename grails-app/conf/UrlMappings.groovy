a3f61ad7471956485b90fc9d37a9820f4a477823
RGBz <rgbz@squeakytinkerings.com>
2014-10-17 03:13:48 -0400

    Added RESTful /api/contacts endpoint to allow for JavaScript-based interaction with the contacts.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/"(controller:"application")
         "/viewcontact"(controller:"application",action:"viewcontact")
@@ -17,5 +12,13 @@ class UrlMappings {
         "/findafriennd" (controllre:"application",action:"listAllUsers")
         "/logout"(controller:"auth", action: 'signOut')
         "500"(view:'/error')
+
+        "/api/contacts"(resources: "contact")
+
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
 	}
 }
---
+++ grails-app/controllers/dude/ContactController.groovy
@@ -0,0 +1,53 @@
+package dude
+
+import grails.rest.RestfulController
+import grails.transaction.Transactional
+import org.apache.shiro.SecurityUtils
+
+class ContactController extends RestfulController {
+
+    static responseFormats = ['json']
+
+    def applicationService
+
+    def index() {
+        respond applicationService.allContacts
+    }
+
+    def show(Contact contact) {
+        if (!contact) {
+            respond status: 404
+        }
+        respond contact
+    }
+
+    @Transactional
+    def update(Contact contact) {
+        if (!applicationService.getContact(contact.id.toString())) {
+            render status: 404
+        }
+        else {
+            contact.save(failOnError: true)
+            respond contact
+        }
+    }
+
+    @Transactional
+    def save(Contact contact) {
+        String username = SecurityUtils.subject.principal
+        contact.user = ShiroUser.findByUsername(username)
+        contact.save(failOnError:true)
+        respond contact
+    }
+
+    def delete(Contact contact) {
+        if (!contact) {
+            render status: 404
+        }
+        else {
+            contact.delete(failOnError: true, flush: true)
+            render status: 200
+        }
+    }
+
+}
